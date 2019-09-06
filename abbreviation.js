bbb4961a485ff60bb816b2cf274c468038f500f1
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-09-06 20:59:25 +0200

    with best solutions

--- abbreviation.js
+++ abbreviation.js
@@ -1,5 +1,6 @@
+//my solution
+
 function abbreviate(string) {
-    
        return  string.split(',').map(x => x.trim())
                                 .map(x => x.split(' ')
                                 .map(y => y.split('-'))
@@ -11,3 +12,18 @@ function abbreviate(string) {
 }
 
 abbreviate('You need, need not want, to complete this code-wars mission')
+
+// best solution
+
+var find = /[a-z]{4,}/gi;
+function replace(match) { return match[0] + (match.length - 2) + match[match.length - 1]; }
+
+function abbreviate(string) {
+    return string.replace(find, replace);
+}
+//__
+function abbreviate(string) {
+    return string.replace(/\w{4,}/g, function (word) {
+        return word[0] + (word.length - 2) + word.slice(-1);
+    });
+}
