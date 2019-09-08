6eeb08495a4f1b2c0303874f801032f8bd461586
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-09-08 14:13:41 +0200

    Anagram 2

--- Anagram.js
+++ Anagram.js
@@ -1,7 +1,39 @@
-function Anagram(str){
+//my solution
 
-    
-    return result 
+function anagramCounter(array){
+    let container = []
+    let counter = 0
+
+    for ( let i = 0; i < array.length; i++ ){
+        container.push(array[i].split('').sort().join('')) 
+    }
+
+    for ( let i = 0; i < container.length; i++ ){
+          for ( let j = i+1; j < container.length; j++ ){
+            container[i] == container[j] ? counter++ : null
+        }
+    }  
+
+    return counter
 }
 
-Anagram
+
+// best solutions 
+function anagramCounter(arrayOfWords) {
+    let sortedWords = arrayOfWords.map(word => word.split('').sort().join(''));
+    let numberOfAnagrams = 0;
+
+    sortedWords.forEach((word, theIndex) => {
+        for (let i = theIndex + 1; i < sortedWords.length; i++) {
+            if (word === sortedWords[i]) {
+                numberOfAnagrams++
+            }
+        }
+    })
+    return numberOfAnagrams
+}
+
+
+anagramCounter = a => (a = a.map(e => e.split('').sort().join(''))).reduce((r, e, i) => (a.forEach((b, j) => r += (i == j ? 0 : e == b ? 1 : 0)), r), 0) / 2;
+
+anagramCounter(['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab'])
