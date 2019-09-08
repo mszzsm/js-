574c8ac25ce4366e6160df95518b00bee0663f25
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-09-08 18:34:26 +0200

    Sum of pirs

--- SumOfPairs_optimized.js
+++ SumOfPairs_optimized.js
@@ -1,27 +1,27 @@
-//my solution  (to slow )
-// var sum_pairs=function(ints, s){
+my solution  (to slow )
+var sum_pairs=function(ints, s){
    
-//   let result = []
+  let result = []
   
-//   ints.map((x,i) => x + ints[i+1] == s ? result.push( x, ints[i+1]) : null ) 
+  ints.map((x,i) => x + ints[i+1] == s ? result.push( x, ints[i+1]) : null ) 
 
-//   const SecondNode = (it, index, arr) => { 
+  const SecondNode = (it, index, arr) => { 
 
-//        for (let i = 0; i < arr.length; i++) {
-//         if(result.length > 0){break} else {
-//         let item = it
-//         let item2 = arr[i+1]
-//         if(item + item2 == s ){ result = [item, item2]}
-//         }
-//        }
-//         return;
-//      }
-//    debugger
-//    result.length > 0 ? null : ints.forEach(SecondNode) 
+       for (let i = 0; i < arr.length; i++) {
+        if(result.length > 0){break} else {
+        let item = it
+        let item2 = arr[i+1]
+        if(item + item2 == s ){ result = [item, item2]}
+        }
+       }
+        return;
+     }
+   debugger
+   result.length > 0 ? null : ints.forEach(SecondNode) 
 
-//    return result.length == 0 ? undefined :  result
+   return result.length == 0 ? undefined :  result
    
-// }
+}
 
 //best solution
 
