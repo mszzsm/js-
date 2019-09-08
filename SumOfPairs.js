5efc83470c55989912ee21a076bc27557dbcd5c6
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-09-08 18:33:47 +0200

    SumOfPairs

--- SumOfPairs.js
+++ SumOfPairs.js
@@ -1,19 +1,20 @@
 var sum_pairs=function(ints, s){
-   
    let result = []
-    debugger
-   for(let i = 0; i < ints.length; i++){
-      
-      if(result.length > 0) {break}
-      for(let j = i+1; j < ints.length; j++){
-        
-        if(ints[i] + ints[j] == s ) { result = [ints[i], ints[j]]; break }
-       
-      }      
-   }
 
-   return result
+   ints.map((x,i) => x + ints[i+1] == s ? result.push( x, ints[i+1]) : null ) 
+       if (result.length == 0) {
+           for (let i = 0; i < ints.length; i++){
+
+              if(result.length > 0) {break}
+              for(let j = i+1; j < ints.length; j++){
+
+                if(ints[i] + ints[j] == s ) { result.push(ints[i], ints[j]); }
+
+              }      
+           }  
+       }
+   return result.length == 0 ? undefined :  result
    
 }
 
-sum_pairs([10,5,2,3,7,5], 10)
+sum_pairs([10, 5, 2, 3, 7, 5], -7)
---
+++ SumOfPairs_optimized.js
@@ -0,0 +1,41 @@
+//my solution  (to slow )
+// var sum_pairs=function(ints, s){
+   
+//   let result = []
+  
+//   ints.map((x,i) => x + ints[i+1] == s ? result.push( x, ints[i+1]) : null ) 
+
+//   const SecondNode = (it, index, arr) => { 
+
+//        for (let i = 0; i < arr.length; i++) {
+//         if(result.length > 0){break} else {
+//         let item = it
+//         let item2 = arr[i+1]
+//         if(item + item2 == s ){ result = [item, item2]}
+//         }
+//        }
+//         return;
+//      }
+//    debugger
+//    result.length > 0 ? null : ints.forEach(SecondNode) 
+
+//    return result.length == 0 ? undefined :  result
+   
+// }
+
+//best solution
+
+function sum_pairs(integers, sum) {
+  debugger
+  var seenNumbers = {};
+  for (var i = 0; i < integers.length; i++) {
+    if (seenNumbers.hasOwnProperty(sum - integers[i])) {
+      return [sum - integers[i], integers[i]];
+    } else {
+      seenNumbers[integers[i]] = true;
+    }
+  }
+  return undefined;
+}
+
+sum_pairs([1,4,8,7,3,15], 8)
